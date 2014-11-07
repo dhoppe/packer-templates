73cfb7b88e624713d5769e8880003ce1d05309e7
Dennis Hoppe <github@debian-solutions.de>
2014-11-07 16:51:35 +0100

    Manage bad cached lists and broken indexes on 12.04.05

--- ubuntu-12.04.5-amd64-nocm/scripts/update.sh
+++ ubuntu-12.04.5-amd64-nocm/scripts/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# manage bad cached lists on 12.04.5
+rm -rf /var/lib/apt/lists
+
 apt-get update
 apt-get upgrade -y
+
+# manage broken package indexes on 12.04.5
+apt-get install dpkg libreadline-dev -y
