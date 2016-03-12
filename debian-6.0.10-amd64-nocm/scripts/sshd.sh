42a4e506c9aec9ec3870dda8a346e647acbdc8e0
Dennis Hoppe <github@debian-solutions.de>
2016-03-12 18:19:01 +0100

    Remove Debian 6.x because of EOL on 2016-02-29

--- debian-6.0.10-amd64-nocm/scripts/sshd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo 'UseDNS no' >> /etc/ssh/sshd_config
--- debian-6.0.10-amd64-nocm/scripts/virtualbox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get install linux-headers-$(uname -r) -y
-
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm /home/vagrant/VBoxGuestAdditions.iso
