309b37fb15ccee9c66b181abf7fcf874abbd7db1
Dennis Hoppe <github@debian-solutions.de>
2016-05-12 17:16:41 +0200

    Fix network issue #7155

--- ubuntu-16.04-amd64-nocm/scripts/vagrant.sh
+++ ubuntu-16.04-amd64-nocm/scripts/vagrant.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sed -i /etc/default/grub \
+  -e "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"biosdevname=0 net.ifnames=0 quiet\"/g"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+sed /etc/network/interfaces -i -e 's/\enp0s3/eth0/g'
+echo 'auto eth1' >> /etc/network/interfaces.d/eth1.cfg
+echo 'iface eth1 inet manual' >> /etc/network/interfaces.d/eth1.cfg
+
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
