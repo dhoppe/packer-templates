dc010eed960beb5902e3ccaba1e77e67ab478797
Dennis Hoppe <github@debian-solutions.de>
2016-05-12 13:21:00 +0200

    Use dist-upgrade to get the latest kernel

--- ubuntu-12.04.5-amd64/scripts/update.sh
+++ ubuntu-12.04.5-amd64/scripts/update.sh
@@ -4,7 +4,10 @@
 rm -rf /var/lib/apt/lists
 
 apt-get update
-apt-get upgrade -y
+apt-get dist-upgrade -y
 
 # manage broken package indexes on 12.04.5
 apt-get install dpkg libreadline-dev -y
+
+reboot
+sleep 60
--- ubuntu-14.04.4-amd64-nocm/scripts/update.sh
+++ ubuntu-14.04.4-amd64-nocm/scripts/update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get upgrade -y
+apt-get dist-upgrade -y
+
+reboot
+sleep 60
