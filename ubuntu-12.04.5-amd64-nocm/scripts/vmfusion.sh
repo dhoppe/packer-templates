d2d7bcfa1a56febe0040d244e81755dd42a9ae35
Dennis Hoppe <github@debian-solutions.de>
2016-09-19 10:26:24 +0200

    Fix issue regarding HGFS

--- ubuntu-12.04.5-amd64-nocm/scripts/vmfusion.sh
+++ ubuntu-12.04.5-amd64-nocm/scripts/vmfusion.sh
@@ -5,7 +5,7 @@ apt-get install linux-headers-$(uname -r) -y
 mkdir -p /mnt/cdrom
 mount -o loop /home/vagrant/linux.iso /mnt/cdrom
 tar xfz /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-/tmp/vmware-tools-distrib/vmware-install.pl -d
+/tmp/vmware-tools-distrib/vmware-install.pl -f
 rm /home/vagrant/linux.iso
 rm -rf /tmp/vmware-*
 umount /mnt/cdrom
